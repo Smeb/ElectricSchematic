a18429b0e032620ed92b4ec45a48ee4f36b301e5
Smeb <bryves@gmail.com>
2016-01-11 14:53:54 +0000

    Implemented draggable components

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,23 +1,40 @@
 package sample;
 
+import components.Component;
+import components.ComponentFactory;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.Cursor;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Group root = new Group();
+        Button button = new Button("New component");
+        button.setOnMouseClicked(new EventHandler<MouseEvent>(){
+            @Override
+            public void handle(MouseEvent event){
+                root.getChildren().add(ComponentFactory.getInstance().newComponent(Component.class, 50, 50));
+            }
+        });
+        root.getChildren().addAll(button);
+        primaryStage.setTitle("Electric Schematic");
+        primaryStage.setScene(new Scene(root, 1000, 700));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
     }
+
+
 }
