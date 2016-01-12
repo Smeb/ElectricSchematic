e8fa9e5bb1d8f4e327076e8a836ab570e06ee481
Henry Mortimer <henry@morti.plus.com>
2016-01-12 11:30:24 +0000

    Refactored wire code

--- src/Controllers/WireController.java
+++ src/Controllers/WireController.java
@@ -1,7 +1,11 @@
 package Controllers;
 
+import components.Anchor;
+import javafx.scene.Group;
+import tools.Wire;
+
 public class WireController {
-    /*
+
     private static WireController instance;
     private Wire wire = null;
 
@@ -12,17 +16,19 @@ public class WireController {
         return instance;
     }
 
-    private WireController() {
-        ;
-    }
-
     public WireController(Wire wire){
         this.wire = wire;
     }
 
-    public WireController(
-            wire = null;
-    )
-    */
+    public WireController(){
+        wire = null;
+    }
+
+    public void addWire(Group root, Anchor start, Anchor end)
+    {
+        Wire aWire = new Wire(start, end);
+        aWire.setStrokeWidth(2);
+        root.getChildren().add(aWire);
+    }
 
 }
