3fbfee2f984fe16fac1e27b737dc74dec47d05dd
Minttu Alakuijala <minttu.alakuijala@gmail.com>
2016-01-12 12:28:26 +0000

    Lamp button in palette

---
+++ src/sample/ComponentColorMap.java
@@ -0,0 +1,28 @@
+package sample;
+
+import components.Component;
+import components.Lamp;
+import javafx.scene.paint.Color;
+
+import java.util.HashMap;
+
+/**
+ * Created by minttu on 12/01/2016.
+ */
+public class ComponentColorMap {
+    private static ComponentColorMap instance = null;
+    private static HashMap<Class, Color> colorMap;
+    public static ComponentColorMap getInstance(){
+        if(instance == null)
+            instance = new ComponentColorMap();
+        return instance;
+    }
+
+    private ComponentColorMap(){
+        colorMap = new HashMap<>();
+        colorMap.put(Lamp.class, Color.YELLOW);
+    }
+    public Color getColor(Class componentType) {
+        return colorMap.get(componentType);
+    }
+}
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,6 +3,7 @@ package sample;
 import components.Component;
 import components.ComponentFactory;
 import components.ComponentGroupFactory;
+import components.Lamp;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -17,12 +18,13 @@ import java.util.ArrayList;
 
 public class Main extends Application {
     private Palette createPalette() {
-        ArrayList<Component> tools = new ArrayList<Component>();
-        for (int i = 0; i < 5; ++i) {
+        ArrayList<Class> tools = new ArrayList<Class>();
+        tools.add(Lamp.class);
+        /*for (int i = 0; i < 5; ++i) {
             tools.add(new Component(30,50, Color.YELLOW));
             tools.add(new Component(30,50, Color.BLUE));
             tools.add(new Component(30,50, Color.GREEN));
-        }
+        }*/
         Palette pal = new Palette(30,50,200,40,4,tools);
         return pal;
     }
@@ -33,7 +35,6 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         Group root = new Group();
 
         ComponentGroupFactory.setWorkspace(root);
@@ -42,8 +43,13 @@ public class Main extends Application {
         Palette pal = createPalette();
         Button button = new Button("New component");
         button.setOnMouseClicked((event) -> ComponentFactory.getInstance().newComponent(Component.class, 50, 50));
-        root.getChildren().addAll(button);
-        root.getChildren().add(pal);
+        /*Button lampButton = new Button("New lamp");
+        lampButton.setLayoutX(150);
+        lampButton.setLayoutY(0);
+        lampButton.setOnMouseClicked((event) -> ComponentFactory.getInstance().newComponent(Lamp.class, 150, 150));
+        root.getChildren().addAll(lampButton);
+        */root.getChildren().addAll(button);
+        root.getChildren().addAll(pal);
         primaryStage.setTitle("Electric Schematic");
         primaryStage.setScene(new Scene(root, 1000, 700));
         primaryStage.show();
