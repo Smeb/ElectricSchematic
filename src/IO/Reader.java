2adaf2e0cb3bca350df8edb889aa2b97561e96e0
Henry Mortimer <henry@morti.plus.com>
2016-01-12 18:11:08 +0000

    Commit file properly

---
+++ src/IO/Reader.java
@@ -0,0 +1,40 @@
+package IO;
+
+import java.io.FileInputStream;
+import org.apache.commons.io.IOUtils;
+
+import org.json.JSONObject;
+
+/**
+ * Created by henrymortimer on 12/01/2016.
+ */
+public class Reader
+{
+    private static Reader instance = null;
+    protected Reader() {
+        // Exists only to defeat instantiation.
+    }
+    public static Reader getInstance() {
+        if(instance == null) {
+            instance = new Reader();
+        }
+        return instance;
+    }
+
+    public JSONObject read(String file)
+    {
+        String jsonTxt;
+        FileInputStream inputStream;
+        JSONObject input;
+        try {
+            inputStream = new FileInputStream(file);
+            jsonTxt = IOUtils.toString(inputStream);
+            input = new JSONObject(jsonTxt);
+        }catch(Exception e) {
+            System.err.println(e);
+            return null;
+        }
+        return input;
+    }
+
+}
