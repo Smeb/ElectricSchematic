657a58b60934134339453deb4c957ad89561a767
Smeb <bryves@gmail.com>
2016-01-13 18:15:27 +0000

    Remove ParallelComponentFactory, restructure ParallelComponent

--- src/components/parts/ComponentFactory.java
+++ src/components/parts/ComponentFactory.java
@@ -5,6 +5,8 @@ import components.infrastructure.ComponentView;
 import components.infrastructure.ComponentViewFactory;
 import javafx.scene.Group;
 
+import java.util.Vector;
+
 public class ComponentFactory {
     private static final ComponentFactory instance = new ComponentFactory();
     private static Group workspace = null;
@@ -35,6 +37,31 @@ public class ComponentFactory {
         }
         ComponentRegistry.getInstance().addComponent(component);
         return component;
+    }
+
+    public ParallelComponent newParallelComponent(Class... classType){
+        ComponentViewFactory viewFactory = ComponentViewFactory.getInstance();
+        Component current;
+        ComponentView currentGroup;
+        Vector<Component> components = new Vector<Component>();
+        Vector<ComponentView> componentViews = new Vector<>();
+        for (final Class c : classType) {
+            if (!c.isAssignableFrom(Component.class)) {
+                return null;
+            }
+            current = newComponent(c, 0.0, 0.0, true);
+            components.add(current);
+            currentGroup = viewFactory.buildComponentGroup(current);
+            componentViews.add(currentGroup);
+        }
+        ParallelComponentView componentView = new ParallelComponentView(componentViews);
+        ParallelComponent parallelComponent = new ParallelComponent(components, componentView, false);
+        workspace.getChildren().add(parallelComponent.getGroup());
+        //TODO: Arrangement of ComponentViews within ParallelComponentView
+        //TODO: Arrangement of wires connecting components
+        //TODO: Attachment of anchors to ParallelComponentView
+        //TODO: Registration of interaction events on child views
+        return parallelComponent;
     }
 
 }
