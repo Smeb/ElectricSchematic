78e44bc076259da595ca8c8a6a08ec7f06f70892
Smeb <bryves@gmail.com>
2016-01-13 15:33:37 +0000

    Added protected fields for electrical properties

--- src/components/parts/Component.java
+++ src/components/parts/Component.java
@@ -9,11 +9,17 @@ import java.util.LinkedList;
 public abstract class Component {
     public static final Color OUTLINE = Color.BLACK;
     public static final double OFFSET = 30.0;
-    protected static int id = 0;
+
+    private static int id = 0;
     public final int thisId;
+
+    protected double voltage = 0.0;
+    protected double resistance;
+
     protected String name;
     private ComponentGroup componentGroup;
     private LinkedList<Component> connectedComponents;
+
     protected Component(){
         thisId = id++;
         connectedComponents = new LinkedList<>();
