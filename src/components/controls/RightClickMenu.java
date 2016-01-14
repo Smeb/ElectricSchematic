b494897bd9f2ea0b859e6cddf18f174b11f84103
Minttu Alakuijala <minttu.alakuijala@gmail.com>
2016-01-14 18:17:02 +0000

    Parameters customised for component type

--- src/components/controls/RightClickMenu.java
+++ src/components/controls/RightClickMenu.java
@@ -1,12 +1,11 @@
 package components.controls;
 
 import components.infrastructure.ComponentRegistry;
+import components.parts.Battery;
 import components.parts.Component;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.control.*;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.text.Text;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
 
 import java.util.ArrayList;
 
@@ -15,53 +14,34 @@ import java.util.ArrayList;
  */
 public class RightClickMenu extends ContextMenu {
     Component clickedComponent;
-    ArrayList<Menu> options;
+    ArrayList<EditMenuItem> options;
 
     public RightClickMenu(Component clicked) {
         clickedComponent = clicked;
         addOptions();
     }
     private void addOptions() {
-        /*MenuItem item1 = new MenuItem("Edit");
-        item1.getParentMenu().setOnAction(event ->
-            EditMenuFactory.getInstance().buildEditMenu(clickedComponent,event)
-        );
+        options = new ArrayList<>();
+        Menu edit = new Menu("Edit");
 
-        MenuItem item2 = new MenuItem("Delete");
-        item2.getParentMenu().setOnAction(event ->
-            ComponentRegistry.getInstance().deleteComponent(clickedComponent.thisId)
-        );*/
-        Menu item1 = new Menu("Edit");
-        EditMenuItem property1 = new EditMenuItem("Resistance",0,100,40);
-        EditMenuItem property2 = new EditMenuItem("Voltage",0,20,5);
-        property1.setHideOnClick(false);
-        property2.setHideOnClick(false);
+        if (clickedComponent instanceof Battery) {
+            options.add(new EditMenuItem("Voltage",0,20,5));
+        }
+        else {
+            options.add(new EditMenuItem("Resistance", 0, 100, 40));
+        }
 
-        property1.setOnAction(editEvent -> {
-            System.out.println("Editing resistance");
-            //item1.getItems().add(setting);
-            //item1.show();
+        for (EditMenuItem em : options) {
+            em.setHideOnClick(false);
+        }
+
+        edit.setOnAction(event -> {
+            edit.getItems().addAll(options);
+            edit.show();
         });
-        property1.setOnAction(editEvent -> {
-            System.out.println("Editing voltage");
-            //item1.getItems().add(setting);
-            //item1.show();
-        });
-        /*item1.setOnAction(event ->
-            EditMenuFactory.getInstance().buildEditMenu(clickedComponent,event)
-        );*/
-        item1.setOnAction(event -> {
-            System.out.println("got this far");
-            item1.getItems().addAll(property1,property2);
-            item1.show();
-        });
-        //item1.addEventHandler(MouseEvent.MOUSE_ENTERED);
 
-        MenuItem item2 = new MenuItem("Delete");
-        item2.setOnAction(event -> ComponentRegistry.getInstance().deleteComponent(clickedComponent.thisId));
-        this.getItems().addAll(item1,item2);
-
-        //TODO: custom options
-        // addMenus(clicked.getParentComponent().customOptions);
+        MenuItem delete = new MenuItem("Delete");
+        delete.setOnAction(event -> ComponentRegistry.getInstance().deleteComponent(clickedComponent.thisId));
+        this.getItems().addAll(edit,delete);
     }
 }
