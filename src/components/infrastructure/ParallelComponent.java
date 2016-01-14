5248516c00b329cec884f5d9837906d68382aa38
Smeb <bryves@gmail.com>
2016-01-14 20:48:53 +0000

    Make ParallelComponent constructor public while Factory pattern not implemented

--- src/components/infrastructure/ParallelComponent.java
+++ src/components/infrastructure/ParallelComponent.java
@@ -1,19 +1,24 @@
-package components.parts;
+package components.infrastructure;
 
+import components.parts.Component;
 import datastructures.ComponentValueMap;
 import datastructures.DefaultComponentValues;
 
 import java.util.Vector;
 
 public class ParallelComponent extends Component {
+    public static final int MINCOMPONENTS = 2;
+    public static final int MAXCOMPONENTS = 5;
     private static final DefaultComponentValues componentDefaults = ComponentValueMap.getInstance().get(ParallelComponent.class);
-private Vector<Component> components;
+    private Vector<Component> components;
 
     public ParallelComponent(){
         super(false);
     }
 
-    protected ParallelComponent(Vector<Component>  components, ParallelComponentView componentView, boolean composite){
+
+    // TODO : Currently public, probably shouldn't be
+    public ParallelComponent(Vector<Component>  components, ParallelComponentView componentView, boolean composite){
         super(composite);
         this.components = components;
         this.componentView = componentView;
